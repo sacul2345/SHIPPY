3a36a7ba61fb50fcea7f869386a101eec886f732
sacul2345 <lukesteinbrenner@gmail.com>
2024-07-14 20:09:06 -0500

    improved player movement

--- Scenes/PlayerScript.gd
+++ Scenes/PlayerScript.gd
@@ -5,6 +5,8 @@ extends RigidBody3D
 
 const SENS_MULTI = .005
 const MOVE_MULTI = 50
+const RUNSPEED = 15
+const WALKSPEED = 4
 
 var inputDirection : Vector2
 var moveVector : Vector3
@@ -22,11 +24,23 @@ func _physics_process(delta):
 	inputDirection = Input.get_vector("foward","backward","strafeLeft","strafeRight")
 	moveVector = (camera.transform.basis * Vector3(inputDirection.y,0,inputDirection.x)).normalized()
 	
-	if isOnGround() and linear_velocity.length() < 10:
-		apply_impulse(Vector3(moveVector.x,0,moveVector.z) * delta * MOVE_MULTI) 
+	if isOnGround():
+		linear_damp = 5
+		if linear_velocity.length() < isSprinting():
+			apply_impulse(Vector3(moveVector.x,0,moveVector.z) * delta * MOVE_MULTI)
+		if Input.is_action_just_pressed("jump"):
+			apply_impulse(Vector3(0,5,0))
+	else:
+		linear_damp = .5
 
 func isOnGround():
 	if groundCast.is_colliding():
 		return true
 	else:
 		return false
+
+func isSprinting():
+	if Input.is_action_pressed("sprint"):
+		return RUNSPEED
+	else:
+		return WALKSPEED
